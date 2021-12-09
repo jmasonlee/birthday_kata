9f3d399d012e5227090f5b12fe6497b92a6b0452
jmasonlee <jmasonlee@gmail.com>
2021-12-09 06:25:41 -0700

    Changes by tvo

--- birthdaydomain/test_BirthdayMessageMaker.py
+++ birthdaydomain/test_BirthdayMessageMaker.py
@@ -18,6 +18,19 @@ class GetEmployeeNamesAndBirthdates(ABC):
         pass
 
 
+class SendEmail(ABC):
+    @abstractmethod
+    def do(self, messages):
+        pass
+
+
+class TestSendEmail(SendEmail):
+    email_messages = []
+
+    def do(self, messages):
+        self.email_messages = messages
+
+
 class TestGetEmployeeNameAndBirthdates(GetEmployeeNamesAndBirthdates):
     def __init__(self, employees):
         self.employees = employees
@@ -29,18 +42,24 @@ class TestGetEmployeeNameAndBirthdates(GetEmployeeNamesAndBirthdates):
 class BirthdayMessenger:
     def __init__(self, get_employee_names_and_birthdates, send_email):
         self.get_employee_names_and_birthdates = get_employee_names_and_birthdates
+        self.send_email = send_email
 
     def send_birthday_greeting(self):
         employees = self.get_employee_names_and_birthdates.do()
 
-        messages = BirthdayMessage("", "")
+        messages = []
         for employee in employees:
             is_employee_birthday_today = self.is_birthday_today(employee.birthdate)
             if is_employee_birthday_today:
                 messages.append(BirthdayMessage("Happy birthday!", f"Happy birthday, dear {employee.name}!"))
 
-        return messages
+        self.send_email.do(messages)
 
+    def is_birthday_today(self, birthdate):
+        return birthdate == today()
+
+class DateCompare:
+    pass
 
 
 class Test(TestCase):
@@ -65,9 +84,13 @@ class Test(TestCase):
         self.assertEqual([], send_email.email_messages)
 
     def test_send_birthday_message_to_multiple_employees(self):
-        employee1 = Employee("GeePaw", TODAY)
-        employee2 = Employee("John", datetime.datetime.today())
-        get_employee_name_and_birthdate = TestGetEmployeeNameAndBirthdates([employee1])
+        employee1 = Employee("GeePaw", today())
+        employee2 = Employee("John", today())
+        get_employee_name_and_birthdate = TestGetEmployeeNameAndBirthdates([employee1, employee2])
+        send_email = TestSendEmail()
+
+        BirthdayMessenger(get_employee_name_and_birthdate, send_email).send_birthday_greeting()
+
         self.assertEqual([BirthdayMessage("Happy birthday!", "Happy birthday, dear GeePaw!"),
                           BirthdayMessage("Happy birthday!", "Happy birthday, dear John!")],
                          send_email.email_messages)
